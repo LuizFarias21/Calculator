56cb11b70e21348e5136fa03d83a6307f49c28fe
Luiz Claudio <luizbfarias21@gmail.com>
2022-01-12 08:12:45 -0300

    update

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+let displayValue = document.querySelector('.displayValue');
+const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+for (let digit = 0; digit <= digits.length -1; digit++) {
+    const digitButton = document.querySelector(`.${digits[digit]}`);
+    digitButton.addEventListener('click', () => {
+        let copyDisplayValue = displayValue.textContent.split('');
+        copyDisplayValue.push(`${digit}`);
+        displayValue.textContent = copyDisplayValue.join('');
+    })
+    
+}
+
+const AC_btn = document.querySelector('.AC');
+
+AC_btn.addEventListener('click', () => {
+    displayValue.textContent = '';
+})
+
+const C = document.querySelector('.C');
+
+C.addEventListener('click', () => {
+    let copyDisplayValue = displayValue.textContent.split('');
+    copyDisplayValue.pop();
+    displayValue.textContent = copyDisplayValue.join('');
+})
+
 function add(num1, num2) {
     return num1 + num2;
 }
