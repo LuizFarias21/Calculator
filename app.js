7ea7148714ea7d8bd227783b3886df41740a7e17
Luiz Claudio <luizbfarias21@gmail.com>
2022-02-02 13:19:30 -0300

    added basic functionalities

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+let toggle = false;
+let num1;
+let num2;
+let operator = '';
+
 let displayValue = document.querySelector('.displayValue');
 const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
-for (let digit = 0; digit <= digits.length -1; digit++) {
+for (let digit = 0; digit <= digits.length - 1; digit++) {
     const digitButton = document.querySelector(`.${digits[digit]}`);
     digitButton.addEventListener('click', () => {
+        if (toggle) displayValue.textContent = '';
+        toggle = false;
         let copyDisplayValue = displayValue.textContent.split('');
         copyDisplayValue.push(`${digit}`);
         displayValue.textContent = copyDisplayValue.join('');
+        num1 = Number(displayValue.textContent);
     })
-    
 }
 
 const AC_btn = document.querySelector('.AC');
@@ -17,20 +24,56 @@ AC_btn.addEventListener('click', () => {
     displayValue.textContent = '';
 })
 
-const C = document.querySelector('.C');
+const C_btn = document.querySelector('.C');
 
-C.addEventListener('click', () => {
+C_btn.addEventListener('click', () => {
     let copyDisplayValue = displayValue.textContent.split('');
     copyDisplayValue.pop();
     displayValue.textContent = copyDisplayValue.join('');
 })
+
+const result_btn = document.querySelector('.result');
+
+result_btn.addEventListener('click', () => {
+
+    calculate();
+    operator = '';
+})
+
+const funcs = [add, subtract, multiply, divide, percentage];
+
+for (let func of funcs) {
+
+    let operator_btn = document.querySelector(`.${func.name}`);
+
+    operator_btn.addEventListener('click', () => {
+        if (operator !== '') {
+            calculate();
+            operator = func;
+
+        } else {
+            operator = func;
+            toggle = true;
+            num2 = Number(displayValue.textContent);
+        }
+    })
+}
+
+function calculate() {
+
+    displayValue.textContent = operate(operator, num1, num2);
+    num2 = Number(displayValue.textContent);
+
+    toggle = true;
+
+}
 
 function add(num1, num2) {
     return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return num2 - num1;
 }
 
 function multiply(num1, num2) {
@@ -38,10 +81,15 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return num2 / num1;
 }
 
-function operate(operator) {
-    return operator;
+function percentage(num1, num2) {
+    return num2/100 * num1;
 }
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
+
 
